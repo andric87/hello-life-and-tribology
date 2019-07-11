c6a97185032e4cb96768eecd1da9355577376140
andric87 <predrag.andric@skf.com>
2019-07-11 22:39:32 +0200

    rotate_m.m has been eddited

--- rotate_m.m
+++ rotate_m.m
@@ -1,8 +1,8 @@
 %------Function lambda_theta rotates lambda depending
 %------ of the slip plane inclination angle
 function [lambda_theta] = rotate_m(lambda,theta)
-c=cosd(theta);
-s=sind(theta);
-R = [c s 0;-1*s c 0;0 0 1];
+cc=cosd(theta);
+ss=sind(theta);
+R = [cc ss 0;-1*ss cc 0;0 0 1];
 lambda_theta=R*lambda*transpose(R);
 
